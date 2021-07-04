0f039d63daddd0319cc72a13b9f56155deb82c3a
bndw <ben@bdw.to>
2021-07-04 16:45:15 -0700

    Format code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import React from 'react';
-import Card from './components/Card';
+import { Card } from './components/Card';
 import './style.css';
 
 function App() {
   return (
     <div className="App">
-
-      <h1><span role="img" aria-label="antenna-bars">📶</span>&nbsp; WiFi Card</h1>
+      <h1>
+        <span role="img" aria-label="antenna-bars">
+          📶
+        </span>
+        &nbsp; WiFi Card
+      </h1>
 
       <p className="tag">
-        Print a simple card with your WiFi login details. Tape it to the fridge, keep it in your wallet, etc.
+        Print a simple card with your WiFi login details. Tape it to the fridge,
+        keep it in your wallet, etc.
       </p>
 
       <p className="tag">
-        Your WiFi information is never sent to the server.
-        No tracking, analytics, or fingerprinting are used on this website.
-        View the <a href="https://github.com/bndw/wifi-card">source code</a>.
+        Your WiFi information is never sent to the server. No tracking,
+        analytics, or fingerprinting are used on this website. View the{' '}
+        <a href="https://github.com/bndw/wifi-card">source code</a>.
       </p>
 
       <Card />
-
     </div>
   );
 }
