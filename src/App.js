b0ee18a9c0f16517aa87f8b58df55286af2f04f1
KeJun <kejun1997@gmail.com>
2021-07-17 23:03:14 +0800

    feat: add language detector (#67)
    * feat: add language detector
    
    * style: fmt code

--- src/App.js
+++ src/App.js
@@ -16,9 +16,12 @@ function App() {
 
       <div>
         <label>{t('select')}</label>
-        <select onChange={(e) => i18n.changeLanguage(e.target.value)}>
+        <select
+          value={i18n.language}
+          onChange={(e) => i18n.changeLanguage(e.target.value)}
+        >
           <option value="en-US">en-US</option>
-          <option value="简体中文">简体中文</option>
+          <option value="zh-CN">简体中文</option>
           <option value="es">es</option>
           <option value="pt">Português</option>
         </select>
