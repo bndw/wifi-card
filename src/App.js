03851f758bdf6073d3345efab30935ccc65cf26a
YuetAu <12145645+YuetAu@users.noreply.github.com>
2021-07-20 18:33:27 +0800

    Update switch

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ function App() {
           onChange={(e) => i18n.changeLanguage(e.target.value)}
         >
           <option value="en-US">en-US</option>
-          <option value="zh-HK">简体中文</option>
+          <option value="zh-HK">繁體中文🇭🇰</option>
           <option value="zh-CN">简体中文</option>
           <option value="es">es</option>
           <option value="pt">Português</option>
