7b5d904fe311fcbfc9dd1c05cc1dd56d59b71706
Ryan Aunur <ryandevstudio@gmail.com>
2021-07-27 15:14:53 +0700

    feat(i18n): add Indonesia language

--- src/App.js
+++ src/App.js
@@ -47,6 +47,7 @@ function App() {
           <option value="fr-FR">French - FR (Français)</option>
           <option value="pt-BR">pt-BR</option>
           <option value="tr-TR">Turkish (Türkçe)</option>
+          <option value="id-ID">Bahasa Indonesia</option>
         </select>
       </div>
 
