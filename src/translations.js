8e9897301a217a2814fa7a32f1bfda63459d7ff2
bndw <ben@bdw.to>
2021-12-28 20:38:39 -0800

    Remove duplicate keys

--- src/translations.js
+++ src/translations.js
@@ -542,10 +542,6 @@ export const Translations = [
       'wifi.password.placeholder': 'Password',
       'wifi.password.hide': 'Nascondi password',
       'wifi.name.hiddenSSID': 'SSID nascosto',
-      'wifi.alert.password.length.5':
-        'La password deve avere almeno 5 caratteri, o cambia il tipo di crittografia a "Nessuno"',
-      'wifi.alert.password.length.8':
-        'La password deve avere almeno 8 caratteri, o cambia il tipo di crittografia a "Nessuno"',
       'wifi.password.encryption': 'Cifratura',
       'wifi.password.encryption.none': 'Nessuno',
       'wifi.tip':
