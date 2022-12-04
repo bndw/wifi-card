de3002e146f3d9df672f35f94cd1b8567d5b87e2
Ramin <r.rezaie@digikala.com>
2022-12-04 13:46:11 +0330

    feat: add node-to-image option

---
+++ src/nodeToImageDownloader.js
@@ -0,0 +1,18 @@
+import { toPng } from 'html-to-image';
+
+const downloader = require('downloadjs');
+
+export const nodeToImageDownloader = ({
+  nodeId,
+  imageName,
+  filter = (node) => node
+}) => {
+  if (!nodeId || !imageName) return;
+  toPng(document.getElementById(nodeId), { filter })
+    .then((dataUrl) => {
+      downloader(dataUrl, `${imageName}.png`);
+    })
+    .catch(() => {
+      console.error("ERROR: the image downloader crashed.")
+    });
+};
